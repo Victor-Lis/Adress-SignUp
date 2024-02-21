ac61d4deeae28343218c0c3ac85a826ef7b7d449
Victor-Lis <victorlisbronzo1@gmail.com>
2024-02-21 20:34:23 -0300

    v1.05
    - Iniciando tela Adresses

---
+++ src/app/adresses/page.tsx
@@ -0,0 +1,7 @@
+export default function Adresses() {
+  return (
+    <main className="flex items-center flex-col justify-center min-h-[calc(100vh-90px)] bg-zinc-900">
+      <h2 className="text-white">Adresses</h2>
+    </main>
+  );
+}
