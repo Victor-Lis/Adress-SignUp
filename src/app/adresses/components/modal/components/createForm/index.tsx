804a3c899f83da7ef7dd2d9055229376ede970db
Victor-Lis <victorlisbronzo1@gmail.com>
2024-02-22 20:46:28 -0300

    v1.17
    - Criando useStates e atribuindo aos campos.

--- src/app/adresses/components/modal/components/createForm/index.tsx
+++ src/app/adresses/components/modal/components/createForm/index.tsx
@@ -38,6 +38,16 @@ export default function CreateForm({
     "TO",
   ];
 
+  const [CEP, setCEP] = useState<string>("");
+  const [logradouro, setLogradouro] = useState<string>("");
+  const [localidade, setLocalidade] = useState<string>("");
+  const [UF, setUF] = useState<string>("");
+
+  const [bairro, setBairro] = useState<string>("");
+  const [siafi, setSiafi] = useState<string>("");
+  const [ibge, setIbge] = useState<string>("");
+  const [DDD, setDDD] = useState<string>("");
+
   const route = useRouter();
 
   const [loading, setLoading] = useState(false);
@@ -46,6 +56,8 @@ export default function CreateForm({
     e.preventDefault();
     setLoading(false);
   }
+
+
 
   return (
     <>
@@ -65,6 +77,8 @@ export default function CreateForm({
               minLength={9}
               maxLength={9}
               required
+              value={CEP}
+              onChange={(e) => setCEP(e.target.value)}
             />
           </div>
           <div className="flex justify-center items-center my-1 flex-wrap flex-1">
@@ -75,6 +89,7 @@ export default function CreateForm({
               title="Apenas letras"
               className="outline-none bg-slate-100 rounded px-2 py-1"
               required
+              value={logradouro}
             />
           </div>
           <div className="flex justify-center items-center my-1 flex-wrap flex-1">
@@ -85,29 +100,31 @@ export default function CreateForm({
               title="Apenas letras"
               className="outline-none bg-slate-100 rounded px-2 py-1"
               required
+              value={localidade}
             />
           </div>
           <div className="flex flex-col justify-center items-center my-1 flex-wrap flex-1">
             <h2 className="text-2xl text-black mx-2">UF</h2>
-            <select className="outline-none bg-slate-100 rounded px-2 py-1">
-              {estados.map((uf) => (
-                <option value={uf}>{uf}</option>
-              ))}
+            <select className="outline-none bg-slate-100 rounded px-2 py-1" onChange={(e) => setUF(e.target.value)}>
+              <option value={UF}>{UF}</option>
+              {estados.map((uf) => {
+                if(uf !== UF) return <option key={uf} value={uf}>{uf}</option>
+              })}
             </select>
           </div>
         </div>
         <div className="flex justify-around items-center w-full flex-wrap my-2">
           <h2 className="my-2 mx-3 text-center flex-1">
-            <strong>Bairro:</strong>{" "}
+            <strong>Bairro:</strong>{" "}<br/>{bairro}
           </h2>
           <h2 className="my-2 mx-3 text-center flex-1">
-            <strong>IBGE:</strong>{" "}
+            <strong>IBGE:</strong>{" "}<br/>{ibge}
           </h2>
           <h2 className="my-2 mx-3 text-center flex-1">
-            <strong>SIAFI:</strong>{" "}
+            <strong>SIAFI:</strong>{" "}<br/>{siafi}
           </h2>
           <h2 className="my-2 mx-3 text-center flex-1">
-            <strong>DDD:</strong>{" "}
+            <strong>DDD:</strong>{" "}<br/>{DDD}
           </h2>
         </div>
         <button
