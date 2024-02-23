38c2aed148fbc67396c4af3e70103c4481394502
Victor-Lis <victorlisbronzo1@gmail.com>
2024-02-22 21:25:14 -0300

    v1.21
    - Privando rota "/adresses";
    - Apagando arquivo functions.ts

--- src/app/adresses/page.tsx
+++ src/app/adresses/page.tsx
@@ -1,7 +1,16 @@
 import Header from "./components/header";
+import { authOptions } from '@/lib/auth'
+import { getServerSession } from 'next-auth'
 import { ModalProvider } from "./providers/modalProvider";
+import { redirect } from "next/navigation";
 
-export default function Adresses() {
+export default async function Adresses() {
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    redirect("/")
+  }
+
   return (
     <ModalProvider>
       <main className="flex flex-col justify-start min-h-[calc(100vh-90px)] bg-zinc-900">
