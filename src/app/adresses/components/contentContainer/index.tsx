56af233b7fff60b8426bf96e5810e80daa8fd766
Victor-Lis <victorlisbronzo1@gmail.com>
2024-02-22 21:55:18 -0300

    v1.25
    - Ajustando UI

--- src/app/adresses/components/contentContainer/index.tsx
+++ src/app/adresses/components/contentContainer/index.tsx
@@ -20,7 +20,7 @@ export default async function contentContainer() {
       },
     })
  return (
-   <div className="w-9/12 mx-auto mt-7 mb-5 grid grid-cols-2 gap-x-5 gap-y-2">
+   <div className="w-9/12 mx-auto mt-4 mb-5 grid grid-cols-2 gap-x-5 gap-y-2">
     {enderecos.map((adress) => {
         return <AdressCard key={adress.cep} adress={adress as AdressType}/>
     })}
--- src/app/adresses/components/header/index.tsx
+++ src/app/adresses/components/header/index.tsx
@@ -8,8 +8,8 @@ import { ModalContext } from "../../providers/modalProvider";
 export default function Header() {
   const { handleModalVisible } = useContext(ModalContext)
   return (
-    <header className="w-9/12 mx-auto flex justify-between items-center mt-7 mb-5">
-      <h2 className="text-blue-600 text-3xl">Endereços</h2>
+    <header className="w-9/12 mx-auto flex justify-between items-center mt-7">
+      <h2 className="text-blue-600 text-4xl">Endereços</h2>
       <FiPlusCircle
         className="hover:scale-110 cursor-pointer duration-300"
         color="#00ff00"
