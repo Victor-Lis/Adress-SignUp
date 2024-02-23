28f508ed771712ee8c62f8e74b4c995b7b9a98eb
Victor-Lis <victorlisbronzo1@gmail.com>
2024-02-22 21:18:11 -0300

    v1.20
    - Atualizando prisma e tipagem

--- src/@types/AdressType.ts
+++ src/@types/AdressType.ts
@@ -1,11 +1,10 @@
-type AdressType = {
+export type AdressType = {
   cep?: string;
   logradouro: string;
   bairro?: string;
   localidade: string;
   uf: string;
   ibge?: string;
-  gia?: string;
   ddd?: string;
   siafi?: string;
 };
