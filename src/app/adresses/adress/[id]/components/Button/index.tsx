4b5cb71c02177bae685c3720d4e77be27d7c4500
Victor-Lis <victorlisbronzo1@gmail.com>
2024-02-25 20:05:12 -0300

    v1.31
    - Criando componente button e chamando função deleteAdress

--- src/app/adresses/adress/[id]/components/Button/index.tsx
+++ src/app/adresses/adress/[id]/components/Button/index.tsx
@@ -1,9 +1,25 @@
 "use client"
 import { FiTrash2 } from "react-icons/fi";
+import { deleteAdress } from "./deleteAdress";
+import { useRouter } from "next/navigation";
 
-export default function Button() {
+type Props = {
+  id: string;
+}
+
+export default function Button({id}: Props) {
+
+  const router = useRouter()
+
+  async function handleDeleteAdress(){
+    let data = await deleteAdress(id)
+    if(data){
+      router.refresh()
+    }
+  }
+
   return (
-    <button className="flex text-white mx-auto">
+    <button className="mt-10 flex text-white mx-auto bg-red-600 px-5 py-2 rounded hover:scale-105 duration-300" onClick={handleDeleteAdress}>
       <FiTrash2 size={25} color="#fff" className="mr-2" />
       Apagar
     </button>
