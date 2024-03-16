1cdb59fed2318dd07c4cca25ac6a87a32718d16c
Victor-Lis <victorlisbronzo1@gmail.com>
2024-03-16 15:56:49 -0300

    v1.51
    - Importando componentes na tela adress

--- src/app/adresses/adress/[id]/page.tsx
+++ src/app/adresses/adress/[id]/page.tsx
@@ -10,6 +10,10 @@ import { WeatherType } from "@/@types/WeatherType";
 
 import { MdOutlineSubtitles } from "react-icons/md";
 import { FaTemperatureArrowDown, FaTemperatureArrowUp, FaTemperatureHalf } from "react-icons/fa6";
+import Header from "./components/Header";
+import { AdressType } from "@/@types/AdressType";
+import AdressComponent from "./components/AdressComponent";
+import WeatherComponent from "./components/WeatherComponent";
 
 type Params = {
   id: string | undefined;
@@ -47,63 +51,12 @@ export default async function Adress({ params }: { params: Params }) {
     city: adress.localidade,
   });
 
-  const firstLetterUpperCase = (string: string) =>
-    string[0].toUpperCase() + string.slice(1, string.length).toLowerCase();
-
   return (
     <main className="w-full flex flex-col justify-start min-h-[calc(100vh-90px)] bg-zinc-900">
       <div className="w-11/12 mx-auto p-5">
-        <header className="w-full flex items-center justify-start">
-          <h2 className="text-blue-600 text-3xl py-2 px-5 bg-white rounded">
-            {adress.cep}
-          </h2>
-        </header>
-        <div
-          style={{ borderTopLeftRadius: 10, borderTopRightRadius: 10 }}
-          className="flex justify-center items-center flex-wrap bg-white px-5 py-2 mt-2"
-        >
-          <div className="flex flex-col justify-center items-start mt-5 flex-1">
-            <h3 className="text-items-start p-2 rounded"><strong>Logradouro </strong>{adress.logradouro}</h3>
-            <h3 className="text-items-start p-2 rounded"><strong>Bairro </strong>{adress.bairro}</h3>
-            <h3 className="text-items-start p-2 rounded"><strong>Localidade </strong>{adress.localidade}</h3>
-            <h3 className="text-items-start p-2 rounded"><strong>Estado </strong>{adress.uf}</h3>
-            <h3 className="text-items-start p-2 rounded"><strong>DDD </strong>{adress.ddd}</h3>
-            <h3 className="text-items-start p-2 rounded"><strong>IBGE </strong>{adress.ibge}</h3>
-            <h3 className="text-items-start p-2 rounded"><strong>SIAFI </strong>{adress.siafi}</h3>
-          </div>
-          <iframe
-            className="flex-1 h-full min-h-[250px] max-[400px]:hidden border-black border-[1.5px]"
-            id="gmap_canvas"
-            src={`https://maps.google.com/maps?q=${adress.bairro}%2C+${adress.localidade}&t=&z=20&ie=UTF8&iwloc=&output=embed`}
-          ></iframe>
-        </div>
-        <div
-          style={{ borderBottomLeftRadius: 10, borderBottomRightRadius: 10 }}
-          className="flex justify-center items-center flex-wrap bg-white px-5 py-2"
-        >
-          <div className="flex flex-wrap justify-center items-start mt-5 flex-1">
-          <div className="flex items-center justify-center mx-2">
-            <MdOutlineSubtitles size={25} color="#3b3b3b" className="mx-1"/>
-            <h3 className="text-items-start py-2 rounded"><strong>Descrição </strong>{firstLetterUpperCase(weather?.weather[0].description as string)}</h3>
-          </div>
-            <div className="flex items-center justify-center mx-2">
-              <FaTemperatureArrowDown size={25} color="#5552df" className="mx-1"/>
-              <h3 className="text-items-start py-2 rounded"><strong>Temperatura Mínima </strong>{weather?.main.temp_min as number}°C</h3>
-            </div>
-            <div className="flex items-center justify-center mx-2">
-              <FaTemperatureArrowUp size={25} color="#e0be70" className="mx-1"/>
-              <h3 className="text-items-start py-2 rounded"><strong>Temperatura Máxima </strong>{weather?.main.temp_max as number}°C</h3>
-            </div>
-            <div className="flex items-center justify-center mx-2">
-              <FaTemperatureHalf size={25} color="#ff0000"/>
-              <h3 className="text-items-start py-2 rounded"><strong>Temperatura </strong>{weather?.main.temp as number}°C</h3>
-            </div>
-            <div className="flex items-center justify-center mx-2">
-              <FaTemperatureHalf size={25} color="#7fc8a8" className="mx-1"/>
-              <h3 className="text-items-start py-2 rounded"><strong>Sensação Térmica </strong>{weather?.main.feels_like as number}°C</h3>
-            </div>
-          </div>
-        </div>
+        <Header adress={adress as AdressType}/>
+        <AdressComponent adress={adress as AdressType}/>
+        <WeatherComponent weather={weather as WeatherType | undefined}/>
         <Button id={params.id} />
       </div>
     </main>
