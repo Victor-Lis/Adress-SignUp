14493ab7a833303053d437ecbcc8b6a3b8fad53c
Victor-Lis <victorlisbronzo1@gmail.com>
2024-03-16 23:16:14 -0300

    v1.57
    - Formulando API

--- src/app/adresses/adress/[id]/components/FinanceComponent/index.tsx
+++ src/app/adresses/adress/[id]/components/FinanceComponent/index.tsx
@@ -1,7 +1,27 @@
-export default function FinanceComponent() {
- return (
-   <div>
+import { FinanceType } from "@/@types/FinanceType";
 
-   </div>
- );
+export default async function FinanceComponent() {
+  async function getDatas() {
+    const url =
+    'https://real-time-finance-data.p.rapidapi.com/market-trends?trend_type=CURRENCIES&country=br&language=pt_br';
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "b7c4441c2emsh836178972adaec9p19716djsnde3452a6651c",
+        "X-RapidAPI-Host": "real-time-finance-data.p.rapidapi.com",
+      },
+    };
+    try {
+      const response = await fetch(url, options);
+      const result: FinanceType = await response.json();
+      result.data.trends = result?.data.trends.filter((trend) => trend.from_symbol === 'USD' || trend.from_symbol === 'EUR');
+      return result
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const result = await getDatas();
+
+  return <div></div>;
 }
